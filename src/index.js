7a246797e610251d74575e405cf2777ba5bf5994
Nicholas March <n.march213@gmail.com>
2016-12-07 07:07:25 -0600

    search bar added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Create a new component. This component should produce some html.
+import SearchBar from './components/search_bar';
 
+const API_KEY = 'AIzaSyCapQitz6Oq47lisli7eQViw97RY_UaarA';
+
+// Create a new component. This component should produce some html.
 const App = () => {
-	return <div>Hi!</div>;
-}
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+};
 
 // Take this components html and put it on the page
 ReactDOM.render(<App />, document.querySelector('.container'));
